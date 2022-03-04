7317e08e167bd445626de8ffdd3789e35afd06ec
korme <korme6868@gmail.com>
2022-03-03 20:26:45 -0800

    Initial commit

---
+++ app/src/main/java/com/kkedir/flashcardapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.kkedir.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        //findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+        //findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
